16dc22943d584928e83c2b26983df2ec12922f2f
ananthvk <123456789987654321sr@gmail.com>
2023-08-21 19:39:28 +0530

    reorganization

---
+++ four_color_image.cpp
@@ -0,0 +1,48 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+#include <iostream>
+int main()
+{
+    const int channels = 3;
+    const int width = 100;
+    const int height = 100;
+    unsigned char *data = new unsigned char[width * height * channels]();
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            // R G
+            // B W
+            if (i <= height / 2 && j <= width / 2)
+            {
+                data[(i * width + j) * channels] = 255;
+                data[(i * width + j) * channels + 1] = 0;
+                data[(i * width + j) * channels + 2] = 0;
+            }
+            else if (i <= height / 2 && j > width / 2)
+            {
+                data[(i * width + j) * channels] = 0;
+                data[(i * width + j) * channels + 1] = 255;
+                data[(i * width + j) * channels + 2] = 0;
+            }
+            else if (i > height / 2 && j <= width / 2)
+            {
+                data[(i * width + j) * channels] = 0;
+                data[(i * width + j) * channels + 1] = 0;
+                data[(i * width + j) * channels + 2] = 255;
+            }
+            else if (i > height / 2 && j > width / 2)
+            {
+                data[(i * width + j) * channels] = 255;
+                data[(i * width + j) * channels + 1] = 255;
+                data[(i * width + j) * channels + 2] = 255;
+            }
+        }
+    }
+
+    stbi_write_png("test.png", width, height, channels, data, width * channels);
+    delete data;
+    return 0;
+}
